a9c1c1498b7d1a5d34fa431b5b8a8f3e663697f4
Truong <nguyenvantruong9876@gmail.com>
2022-07-20 16:15:41 +0700

    du an moi

---
+++ js/main.js
@@ -0,0 +1,172 @@
+//mảng rỗng
+var nums = [];
+
+
+function themSo() {
+    var num = Number(document.querySelector("#formNumber input").value);
+
+    // push : lưu giá trị mới vào mảng
+
+    nums.push(num);
+    console.log(nums);
+    document.getElementById("txtResult1").innerHTML = "Mảng: " + nums;
+}
+document.querySelector("#formNumber button").onclick = themSo;
+//bài1
+function tongChan() {
+    var sum = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            sum += nums[i]
+        }
+    }
+    console.log(sum)
+    document.getElementById("txtResulttong").innerHTML = "Tổng số dương: " + sum;
+}
+document.getElementById("btnTinh").onclick = tongChan;
+//bài 2
+function demSoDuong() {
+    var count = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            count++
+        }
+    }
+    console.log(count);
+    document.getElementById("txtResultduong").innerHTML = "số Dương: " + count;
+}
+document.getElementById("btndem").onclick = demSoDuong;
+//bài 3
+function timmin() {
+    var vitri = 0;
+    var min = nums[0];
+    for (var i = 0; i < nums.length; i++) {
+        if (min > nums[i]) {
+
+            min = nums[i];
+            vitri = i;
+
+        }
+    }
+    document.querySelector("#txtResultnhonhat").innerHTML = "Số nhỏ nhất: " + min;
+}
+document.getElementById("btnnhonhat").onclick = timmin;
+
+//bài 4
+function timsoduongnhonhat() {
+    var vitri = 0;
+    var min = nums[0];
+    for (var i = 0; i < nums.length; i++) {
+        if (min > nums[i]) {
+
+            min = nums[i];
+            vitri = i;
+
+        }
+    }
+    document.querySelector("#txtResultduongnhonhat").innerHTML = "Số dương nhỏ nhất: " + min;
+}
+document.getElementById("btnduongnhonhat").onclick = timsoduongnhonhat;
+//bài 5
+function timsoduongcuoicung() {
+
+    var numbercuoicung = -1;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0) {
+            numbercuoicung = nums[i];
+            numbercuoicung;
+        }
+
+    }
+    document.querySelector("#txtResultduongcuoicung").innerHTML = "Số chẵn cuối cùng: " + numbercuoicung;
+}
+document.getElementById("btnduongcuoicung").onclick = timsoduongcuoicung;
+
+//bài6
+function doicho(a, b) {
+    temp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = temp;
+
+
+}
+function doiCHo() {
+    var vitriso1 = document.getElementById("vitri1").value;
+    var vitriso2 = document.getElementById("vitri2").value;
+    doicho(vitriso1, vitriso2);
+
+    document.querySelector('#txtResultdoicho').innerHTML = "Mảng sau khi đổi: " + nums;
+}
+//bài7
+function sapxep() {
+
+    for (var i = 0; i < nums.length - 1; i++) {
+        for (var j = 0; j < nums.length; j++) {
+
+            if (nums[j] > nums[j + 1]) {
+                doicho(j, j + 1)
+            }
+
+        }
+
+    }
+    console.log(nums)
+    document.querySelector('#txtResultsapxep').innerHTML = "Mảng sau khi sắp xếp: " + nums;
+}
+document.querySelector("#btnsapxep").onclick = sapxep;
+
+//bài8
+
+function ktnt(num) {
+    for (var i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) {
+            return false;
+        }
+    return num > 1;
+}
+function nguyenTo() {
+    var nguyento = -1; // trả về -1 khi không có số nguyên tố
+    for (var i = 0; i < nums.length; i++) {
+        if (ktnt(nums[i])) {
+            nguyento = nums[i];
+            break;
+        }
+    }
+    document.getElementById('txtResultnguyento').innerHTML = nguyento;
+}
+document.querySelector("#btnnguyento").onclick = nguyenTo;
+
+//bai9
+var nums2 = [];
+function themsothuc() {
+    var number = Number(document.getElementById("nhapso").value);
+    nums2.push(number);
+    document.querySelector("#txtResultsothuc").innerHTML = nums2;
+}
+document.querySelector("#btsothuc").onclick = themsothuc;
+function demsonguyen() {
+    var count = 0;
+    for (var i = 0; i < nums2.length; i++) {
+        Number.isInteger(nums2[i]) ? count++ : count;
+    }
+    document.getElementById('txtResultdemso').innerHTML = count;
+}
+document.querySelector("#btdemso").onclick = demsonguyen;
+//bai10
+function sosanh() {
+    var countduong = 0;
+    var countam = 0;
+    kq = '';
+  for (var i = 0; i < nums.length; i++)
+  nums[i] > 0 ? countduong++ : nums[i] < 0 && countam++;
+  if (countduong > countam) {
+    kq ='Số dương > Số âm';
+  } else if (countduong < countam) {
+    kq ='Số âm > Số dương';
+  }else{
+    kq ='Số âm = Số dương';
+  }
+  document.getElementById('txtResultsosanh').innerHTML = kq
+
+}
+document.querySelector("#btsosanh").onclick = sosanh;
